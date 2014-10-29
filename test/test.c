345044faa1a9c9637e54917d4789b7907ce0b9ee
Jie-Gang Kuang <jk3735@columbia.edu>
2014-10-29 18:05:51 -0400

    Add a test program to test new sched class

--- test/test.c
+++ test/test.c
@@ -1,4 +1,26 @@
-int main(int argc, char **argv) 
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+struct test_param {
+	int pri;
+};
+
+int main(int argc, char **argv)
 {
+	struct test_param param = {50};
+
+	if (syscall(__NR_sched_setscheduler, getpid(), 6, &param) < 0) {
+		printf("error: %s\n", strerror(errno));
+		return -1;
+	}
+
+	while (1) {
+		printf("test program to sleep...\n");
+		sleep(5);
+	}
+
 	return 0;
 }
