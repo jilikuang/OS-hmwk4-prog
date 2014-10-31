cbdeeb04454f540ad153ac1acb48a22b33ea8c7b
jh3478 <jh3478@columbia.edu>
2014-10-30 20:19:42 -0400

    Add CONFIG_SMP definition and fix compilation issues

--- flo-kernel/kernel/sched/grr.c
+++ flo-kernel/kernel/sched/grr.c
@@ -12,6 +12,12 @@
 #define	M_TRUE	1
 #define M_FALSE	0
 
+/* Prototypes */
+/*****************************************************************************/
+#ifdef CONFIG_SMP
+static int grr_load_balance(struct rq *this_rq);
+#endif	/* CONFIG_SMP */
+
 /* Utility functions */
 /*****************************************************************************/
 static inline struct task_struct *task_of_se(struct sched_grr_entity *grr_se)
@@ -50,7 +56,127 @@ static void grr_unlock(struct grr_rq *p_grr_rq)
 	raw_spin_unlock(p_lock);
 }
 
-/* REAL thing here */
+/* SMP Load balancing things */
+/*****************************************************************************/
+#ifdef CONFIG_SMP
+/*
+* Get the queue with the highest total number of tasks
+* Bo: Need to consider find queue only within a group later
+*/
+static struct rq * grr_find_busiest_queue(const struct cpumask *cpus)
+{
+	struct rq *busiest = NULL;
+	struct rq *rq;
+	unsigned long max_load = 0;
+	int i;
+
+#if 0
+    /* case for handle group */
+#else
+	for_each_cpu(i, cpus) {
+		unsigned long curr_load;
+
+		if (!cpumask_test_cpu(i, cpus))
+			continue;
+
+		rq = cpu_rq(i);
+		curr_load = rq->grr.m_nr_running;
+
+		if (curr_load > max_load) {
+			max_load = curr_load;
+			busiest = rq;
+		}
+	}
+#endif
+	return busiest;
+}
+
+/*
+* Get the queue with the lowest total number of tasks
+* Bo: Need to consider find queue only within a group later
+*/
+static struct rq * grr_find_least_busiest_queue(const struct cpumask *cpus)
+{
+	struct rq *least_busiest = NULL;
+	struct rq *rq;
+	unsigned long min_load = 0;
+	int i;
+
+#if 0
+    /* case for handle group */
+#else
+	for_each_cpu(i, cpus) {
+		unsigned long curr_load;
+
+		if (!cpumask_test_cpu(i, cpus))
+			continue;
+
+		rq = cpu_rq(i);
+		curr_load = rq->grr.m_nr_running;
+
+		if (curr_load < min_load) {
+			min_load = curr_load;
+			least_busiest = rq;
+		}
+	}
+#endif
+	return least_busiest;
+}
+
+/*
+* Whenever, it is time to do load balance, this function will be called.
+* The fuction will get the busiest queue's next eligble task,
+* and put it into least busiest queue.
+* Bo: 
+* Ignore idle CPU to steal task from other CPU. 
+* Ignore group concept.
+*/
+DEFINE_PER_CPU(cpumask_var_t, load_balance_tmpmask);
+
+static int grr_load_balance(struct rq *this_rq)
+{
+        struct rq *busiest_rq;
+        struct rq *target_rq;
+	struct cpumask *cpus = __get_cpu_var(load_balance_tmpmask);
+        BOOL is_task_moved = M_FALSE;
+	
+	cpumask_copy(cpus, cpu_active_mask);
+
+        grr_lock(&this_rq->grr);
+
+        /* get least and most busiest queue */
+        busiest_rq = grr_find_busiest_queue(cpus);
+		
+        if (!(busiest_rq == this_rq))
+                goto __do_nothing;
+
+        target_rq = grr_find_least_busiest_queue(cpus);
+
+        /* make sure load balance will not reverse */
+        if(busiest_rq->grr.m_nr_running > 1 && 
+        	(target_rq->grr.m_nr_running) + 1 < busiest_rq->grr.m_nr_running){
+            /* Here, we will do task moving */
+  			/*
+  			double_lock_balance(busiest_rq, target_rq);
+  			busiest_rq->grr.pick_next_task();
+  			*/
+			is_task_moved = M_TRUE;
+            /* unlock queues locked in find fucntions */ 
+            grr_unlock(&busiest_rq->grr);
+            grr_unlock(&target_rq->grr);
+
+        }
+        /* unlock this queue locked at first place */ 
+        grr_unlock(&this_rq->grr);
+        return is_task_moved;
+
+__do_nothing:
+        grr_unlock(&this_rq->grr);
+        return is_task_moved; 
+}
+#endif
+
+/* scheduler class functions */
 /*****************************************************************************/
 /* init func: 
  *	For each cpu, it will be called once. Thus, the rq is a PER_CPU data 
@@ -65,13 +191,20 @@ void init_grr_rq(struct grr_rq *grr_rq, struct rq *rq)
 	raw_spin_lock_init(&grr_rq->m_runtime_lock);
 }
 
+#ifdef CONFIG_SMP
 static void pre_schedule_grr(struct rq *rq, struct task_struct *prev)
 {
-#if 0
-	if (rq->rt.highest_prio.curr > prev->prio)
-		pull_rt_task(rq);
-#endif
+	/* handle the case when rebalance is on */
+        if (rq->grr.m_need_balance) {
+                
+		/* reset the rq variable */
+		rq->grr.m_need_balance = M_FALSE;
+
+                /* take care of the rebalance here */
+                grr_load_balance(rq);
+        }
 }
+#endif /* CONFIG_SMP */
 
 /*
  * The enqueue_task method is called before nr_running is
@@ -151,121 +284,8 @@ check_preempt_curr_grr(struct rq *rq, struct task_struct *p, int flags)
 #endif
 }
 
-/*
-* Get the queue with the highest total number of tasks
-* Bo: Need to consider find queue only within a group later
-*/
-static struct rq * grr_find_busiest_queue(const struct cpumask *cpus)
-{
-	struct rq *busiest = NULL;
-	struct rq *rq;
-	unsigned long max_load = 0;
-	int i;
 
-#if 0
-    /* case for handle group */
-#else
-	for_each_cpu(i, cpus) {
-		unsigned long curr_load;
 
-		if (!cpumask_test_cpu(i, cpus))
-			continue;
-
-		rq = cpu_rq(i);
-		curr_load = rq->grr.m_nr_running;
-
-		if (curr_load > max_load) {
-			max_load = curr_load;
-			busiest = rq;
-		}
-	}
-#endif
-	return busiest;
-}
-
-/*
-* Get the queue with the lowest total number of tasks
-* Bo: Need to consider find queue only within a group later
-*/
-static struct rq * grr_find_least_busiest_queue(const struct cpumask *cpus)
-{
-	struct rq *least_busiest = NULL;
-	struct rq *rq;
-	unsigned long min_load = 0;
-	int i;
-
-#if 0
-    /* case for handle group */
-#else
-	for_each_cpu(i, cpus) {
-		unsigned long curr_load;
-
-		if (!cpumask_test_cpu(i, cpus))
-			continue;
-
-		rq = cpu_rq(i);
-		curr_load = rq->grr.m_nr_running;
-
-		if (curr_load < min_load) {
-			min_load = curr_load;
-			least_busiest = rq;
-		}
-	}
-#endif
-	return least_busiest;
-}
-
-/*
-* Whenever, it is time to do load balance, this function will be called.
-* The fuction will get the busiest queue's next eligble task,
-* and put it into least busiest queue.
-* Bo: 
-* Ignore idle CPU to steal task from other CPU. 
-* Ignore group concept.
-*/
-DEFINE_PER_CPU(cpumask_var_t, load_balance_tmpmask);
-
-static int grr_load_balance(struct rq *this_rq)
-{
-        struct rq *busiest_rq;
-        struct rq *target_rq;
-		struct cpumask *cpus = __get_cpu_var(load_balance_tmpmask);
-		cpumask_copy(cpus, cpu_active_mask);
-
-        BOOL is_task_moved = M_FALSE;
-
-        grr_lock(&this_rq->grr);
-
-        /* get least and most busiest queue */
-        busiest_rq = grr_find_busiest_queue(cpus);
-		
-        if (!(busiest_rq == this_rq))
-                goto __do_nothing;
-
-        target_rq = grr_find_least_busiest_queue(cpus);
-
-        /* make sure load balance will not reverse */
-        if(busiest_rq->grr.m_nr_running > 1 && 
-        	(target_rq->grr.m_nr_running) + 1 < busiest_rq->grr.m_nr_running){
-            /* Here, we will do task moving */
-  			/*
-  			double_lock_balance(busiest_rq, target_rq);
-  			busiest_rq->grr.pick_next_task();
-  			*/
-			is_task_moved = M_TRUE;
-            /* unlock queues locked in find fucntions */ 
-            grr_unlock(&busiest_rq->grr);
-            grr_unlock(&target_rq->grr);
-
-        }
-        /* unlock this queue locked at first place */ 
-        grr_unlock(&this_rq->grr);
-        return is_task_moved;
-
-__do_nothing:
-        grr_unlock(&this_rq->grr);
-        return is_task_moved; 
-}
 
 /*
  * return the next task to run: select a task in my run queue if there is any
@@ -278,13 +298,7 @@ static struct task_struct *pick_next_task_grr(struct rq *rq)
 #if 1
 	struct task_struct *p = NULL;
 
-	/* handle the case when rebalance is on */
-	if (rq->grr.m_need_balance) {
-		rq->grr.m_need_balance = M_FALSE;	
 	
-		/* take care of the rebalance here */
-		grr_load_balance(rq);
-	}
 
 	if (!rq->nr_running)
 		return NULL;
@@ -377,6 +391,7 @@ static void task_tick_grr(struct rq *rq, struct task_struct *curr, int queued)
 	struct sched_grr_entity *se = &curr->grr;
 	BOOL need_resched = M_FALSE;
 
+#ifdef CONFIG_SMP
 	/* Update statistics */
 	if ((--rq->grr.m_rebalance_cnt) == 0) {
 		/* set flag for rebalanceing & set resched*/
@@ -384,6 +399,7 @@ static void task_tick_grr(struct rq *rq, struct task_struct *curr, int queued)
 		rq->grr.m_need_balance = M_TRUE;
 		need_resched = M_TRUE;
 	}
+#endif
 
 	if (curr->policy != SCHED_GRR)
 		goto __grr_tick_end__;
@@ -471,10 +487,10 @@ const struct sched_class grr_sched_class = {
 	.put_prev_task		= put_prev_task_grr,
 
 #ifdef CONFIG_SMP
-	.select_task_rq		= select_task_rq_grr,
 	.pre_schedule		= pre_schedule_grr,
 
 #if 0
+	.select_task_rq		= select_task_rq_grr,
 	void (*pre_schedule) (struct rq *this_rq, struct task_struct *task);
 	void (*post_schedule) (struct rq *this_rq);
 	void (*set_cpus_allowed)(struct task_struct*, const struct cpumask*);
