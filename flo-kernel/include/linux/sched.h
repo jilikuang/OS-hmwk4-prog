jilikuang/OS-hmwk4-prog

commit 986c44e3752552325974ae74eac544e0f93a18a4

Reference sched_group from rt.c

flo-kernel/kernel/sched/grr.c
@@ -78,6 +78,102 @@ static void grr_unlock(struct grr_rq *p_grr_rq)
 	raw_spin_unlock(p_lock);
 }
 
+void free_grr_sched_group(struct task_group *tg)
+{
+#ifdef CONFIG_SMP
+	int i;
+/*
+	if (tg->grr_se)
+		destroy_rt_bandwidth(&tg->rt_bandwidth);
+*/
+	for_each_possible_cpu(i) {
+		if (tg->grr_rq)
+			kfree(tg->grr_rq[i]);
+		if (tg->grr_se)
+			kfree(tg->grr_se[i]);
+	}
+
+	kfree(tg->grr_rq);
+	kfree(tg->grr_se);
+#endif
+}
+
+#ifdef CONFIG_SMP
+static void init_tg_grr_entry(struct task_group *tg, struct grr_rq *grr_rq,
+		struct sched_grr_entity *grr_se, int cpu,
+		struct sched_grr_entity *parent)
+{
+	struct rq *rq = cpu_rq(cpu);
+/*
+	grr_rq->highest_prio.curr = MAX_RT_PRIO;
+	grr_rq->rt_nr_boosted = 0;*/
+	grr_rq->rq = rq;
+	grr_rq->tg = tg;
+
+	tg->grr_rq[cpu] = grr_rq;
+	tg->grr_se[cpu] = grr_se;
+
+	if (!grr_se)
+		return;
+
+	if (!parent)
+		grr_se->grr_rq = &rq->grr;
+	else
+		grr_se->grr_rq = parent->my_q;
+
+	grr_se->my_q = grr_rq;
+	grr_se->parent = parent;
+	INIT_LIST_HEAD(&grr_se->m_run_list);
+}
+
+int alloc_grr_sched_group(
+		struct task_group *tg, struct task_group *parent)
+{
+	struct grr_rq *grr_rq;
+	struct sched_grr_entity *grr_se;
+	int i;
+
+	tg->grr_rq = kzalloc(sizeof(grr_rq) * nr_cpu_ids, GFP_KERNEL);
+	if (!tg->grr_rq)
+		goto err;
+	tg->grr_se = kzalloc(sizeof(grr_se) * nr_cpu_ids, GFP_KERNEL);
+	if (!tg->grr_se)
+		goto err;
+/*
+	init_rt_bandwidth(&tg->rt_bandwidth,
+			ktime_to_ns(def_rt_bandwidth.rt_period), 0);
+*/
+	for_each_possible_cpu(i) {
+		grr_rq = kzalloc_node(sizeof(struct grr_rq),
+				     GFP_KERNEL, cpu_to_node(i));
+		if (!grr_rq)
+			goto err;
+
+		grr_se = kzalloc_node(sizeof(struct sched_grr_entity),
+				     GFP_KERNEL, cpu_to_node(i));
+		if (!grr_se)
+			goto err_free_rq;
+
+		init_grr_rq(grr_rq, cpu_rq(i));/*
+		grr_rq->rt_runtime = tg->rt_bandwidth.rt_runtime;*/
+		init_tg_grr_entry(tg, grr_rq, grr_se, i, parent->grr_se[i]);
+	}
+
+	return 1;
+
+err_free_rq:
+	kfree(grr_rq);
+err:
+	return 0;
+}
+#else
+int alloc_grr_sched_group(
+		struct task_group *tg, struct task_group *parent)
+{
+	return 1;
+}
+#endif
+
 /* SMP Load balancing things */
 /*****************************************************************************/
 #ifdef CONFIG_SMP

flo-kernel/kernel/sched/sched.h
@@ -130,9 +130,9 @@ struct task_group {
 
 /* CONFIG_GRR_GROUP_SCHED */
 	struct sched_grr_entity **grr_se;
-/* END */
 	/* @lfred */
 	struct grr_rq **grr_rq;
+/* END */
 
 	struct rcu_head rcu;
 	struct list_head list;
@@ -204,6 +204,10 @@ extern int alloc_rt_sched_group(struct task_group *tg, struct task_group *parent
 extern void init_tg_rt_entry(struct task_group *tg, struct rt_rq *rt_rq,
 		struct sched_rt_entity *rt_se, int cpu,
 		struct sched_rt_entity *parent);
+
+extern void free_grr_sched_group(struct task_group *tg);
+extern int alloc_grr_sched_group(
+		struct task_group *tg, struct task_group *parent);
 
 #else /* CONFIG_CGROUP_SCHED */
 

flo-kernel/kernel/sched/core.c
@@ -7321,6 +7321,7 @@ static void free_sched_group(struct task_group *tg)
 {
 	free_fair_sched_group(tg);
 	free_rt_sched_group(tg);
+	free_grr_sched_group(tg);
 	autogroup_free(tg);
 	kfree(tg);
 }
@@ -7339,6 +7340,9 @@ struct task_group *sched_create_group(struct task_group *parent)
 		goto err;
 
 	if (!alloc_rt_sched_group(tg, parent))
+		goto err;
+
+	if (!alloc_grr_sched_group(tg, parent))
 		goto err;
 
 	spin_lock_irqsave(&task_group_lock, flags);

commit 07f40fa4c8a1b996f08a34414916e57a0656126c

Fix CC errors caused by 986c44e

flo-kernel/include/linux/sched.h
@@ -1275,7 +1275,15 @@ struct sched_grr_entity {
 	
 	/* list struct */
 	struct list_head m_rq_list;
+
+	/* Group Scheduling */
+	struct sched_grr_entity	*parent;
+	/* rq on which this entity is (to be) queued: */
+	struct grr_rq *grr_rq;
+	/* rq "owned" by this entity/group: */
+	struct grr_rq *my_q;
 };
+
 /*****************************************************************************/
 
 struct rcu_node;

flo-kernel/kernel/sched/grr.c
@@ -123,7 +123,7 @@ static void init_tg_grr_entry(struct task_group *tg, struct grr_rq *grr_rq,
 
 	grr_se->my_q = grr_rq;
 	grr_se->parent = parent;
-	INIT_LIST_HEAD(&grr_se->m_run_list);
+	INIT_LIST_HEAD(&grr_se->m_rq_list);
 }
 
 int alloc_grr_sched_group(

flo-kernel/kernel/sched/sched.h
@@ -310,6 +310,10 @@ struct grr_rq {
 	/* various flags */
 	int	m_need_resched;
 	int	m_need_balance;
+
+	/* Group Scheduling */
+	struct rq *rq;
+	struct task_group *tg;
 };
 
 /* Real-Time classes' related field in a runqueue: */